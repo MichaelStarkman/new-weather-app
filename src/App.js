8fc9d2a2fd4430c3f015ec5b53ec96c774333066
MichaelStarkman <michaelstarkman@gmail.com>
2022-08-02 13:47:04 -0600

    TempAndDetails

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Inputs from './Components/Inputs';
+import TempAndDetails from './Components/TempAndDetails';
+import TimeAndLocation from './Components/TimeAndLocation';
 // import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 import TopButtons from './Components/TopButtons';
@@ -10,6 +12,9 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
       <Inputs />
+      
+      <TimeAndLocation />
+      <TempAndDetails />
     </div>
   );
 }
