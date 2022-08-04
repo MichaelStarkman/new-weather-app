fb7d2ef2a139e8a93e9d6a77587bd656828a905e
MichaelStarkman <michaelstarkman@gmail.com>
2022-08-04 11:48:38 -0600

    live forecast

--- src/Components/Forecast.jsx
+++ src/Components/Forecast.jsx
@@ -1,70 +1,28 @@
 import React from 'react'
+import { iconUrlFromCode } from '../services/weatherService'
 
-function Forecast({title}) {
+function Forecast({ title, items }) {
   return (
     <div>
-
         <div className="flex items-center justify-start my-6">
             <p className="text-white font-medium uppercase">{title}</p>
         </div>
         <hr className="my-2"/>
-
         <div className="flex flex-row items-center justify-between text-white">
-
-            <div className="flex flex-col items-center justify-center">
-                <p className="font-light text-sm">
-                    4:20 PM 
-                </p>
+            {items.map(item => (
+                <div className="flex flex-col items-center justify-center">
+                <p className="font-light text-sm">{item.title}</p>
                 <img 
-                    src="https://i.imgur.com/2JHPmCK.png" 
+                    src={iconUrlFromCode(item.icon)}
                     className="w-12 my-1"
                     alt="" />
-                <p>69°</p>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-                <p className="font-light text-sm">
-                    4:20 PM 
+                <p className="font-medium">{`${item.temp.toFixed()}°`}
                 </p>
-                <img 
-                    src="https://i.imgur.com/2JHPmCK.png" 
-                    className="w-12 my-1"
-                    alt="" />
-                <p>69°</p>
             </div>
-            <div className="flex flex-col items-center justify-center">
-                <p className="font-light text-sm">
-                    4:20 PM 
-                </p>
-                <img 
-                    src="https://i.imgur.com/2JHPmCK.png" 
-                    className="w-12 my-1"
-                    alt="" />
-                <p>69°</p>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-                <p className="font-light text-sm">
-                    4:20 PM 
-                </p>
-                <img 
-                    src="https://i.imgur.com/2JHPmCK.png" 
-                    className="w-12 my-1"
-                    alt="" />
-                <p>69°</p>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-                <p className="font-light text-sm">
-                    4:20 PM 
-                </p>
-                <img 
-                    src="https://i.imgur.com/2JHPmCK.png" 
-                    className="w-12 my-1"
-                    alt="" />
-                <p>69°</p>
-            </div>
-
+            ))}
         </div>
     </div>
   )
-}
+};
 
 export default Forecast
